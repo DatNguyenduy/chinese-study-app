0f7ae36e64927222b191759a290580843abda94e
DatNguyenduy <71640808+DatNguyenduy@users.noreply.github.com>
2026-03-31 15:21:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,56 @@ function speak(text) {
     utterance.lang = "zh-CN"; // Chinese
     speechSynthesis.speak(utterance);
 }
+let current = 0;
+let score = 0;
+
+function showVocab() {
+    let html = "<h2>Vocabulary</h2>";
+
+    data.vocabulary.forEach(v => {
+        html += `
+        <div onclick="speak('${v.word}')">
+            <p><b>${v.word}</b></p>
+            <p>${v.meaning}</p>
+            <hr>
+        </div>
+        `;
+    });
+
+    document.getElementById("content").innerHTML = html;
+}
+
+function startQuiz() {
+    current = 0;
+    score = 0;
+    showQuestion();
+}
+
+function showQuestion() {
+    let q = data.quiz[current];
+
+    let html = `<h2>${q.question}</h2>`;
+
+    q.options.forEach((opt, index) => {
+        html += `<button class="option" onclick="checkAnswer(${index})">${opt}</button>`;
+    });
+
+    document.getElementById("content").innerHTML = html;
+}
+
+function checkAnswer(index) {
+    let q = data.quiz[current];
+
+    if (index === q.answer) {
+        score++;
+    }
+
+    current++;
+
+    if (current < data.quiz.length) {
+        showQuestion();
+    } else {
+        document.getElementById("content").innerHTML =
+            `<h2>🎉 Score: ${score}/${data.quiz.length}</h2>`;
+    }
+}
